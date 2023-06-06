776eccfa13e093768979291da77138806024d8d3
tsdeepak <tsdeepak@gmail.com>
2023-06-06 13:49:20 +0530

    .

--- model.py
+++ model.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
 from tqdm import tqdm
+import torch.nn.functional as F
+from torchsummary import summary
+from utils import get_correct_predcount
 
 
 class Net(nn.Module):
@@ -23,3 +26,66 @@ class Net(nn.Module):
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
+    
+def model_summary(input_size,device):
+    model = Net().to(device)
+    summary(model, input_size)
+
+def train(model, device, train_loader, optimizer,train_acc,train_losses):
+    model.train()
+    pbar = tqdm(train_loader)
+
+    train_loss = 0
+    correct = 0
+    processed = 0
+
+    for batch_idx, (data, target) in enumerate(pbar):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+
+        # Predict
+        pred = model(data)
+
+        # Calculate loss
+        loss = F.nll_loss(pred, target)
+        train_loss+=loss.item()
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        
+        correct += get_correct_predcount(pred, target)
+        processed += len(data)
+
+        pbar.set_description(desc= f'Train: Loss={loss.item():0.4f} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+
+    train_acc.append(100*correct/processed)
+    train_losses.append(train_loss/len(train_loader))
+
+    return train_acc, train_losses
+
+def test(model, device, test_loader,test_acc,test_losses):
+    model.eval()
+
+    test_loss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(test_loader):
+            data, target = data.to(device), target.to(device)
+
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+
+            correct += get_correct_predcount(output, target)
+
+
+    test_loss /= len(test_loader.dataset)
+    test_acc.append(100. * correct / len(test_loader.dataset))
+    test_losses.append(test_loss)
+
+    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    
+    return test_acc , test_losses
